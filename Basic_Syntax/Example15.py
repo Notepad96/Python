e16603681a58a80f282f81f7409b0b256427a736
Notepad96 <notepad1029@gmail.com>
2021-01-28 20:08:54 +0900

    210128

--- Basic_Syntax/Example15.py
+++ Basic_Syntax/Example15.py
@@ -1,5 +1,6 @@
 import theater_module as tt
-# from theater_module import price_baby
+# from theater_module import price_bab
+# price_baby(6)
 
 tt.price(4)         # 개당 20000
 tt.price_baby(3)    # 개당 5000
