fc7e8ab7ff9ba1fea0b5c2bca3fae0aa2f2e7d61
Notepad96 <notepad1029@gmail.com>
2021-01-31 04:33:05 +0900

    210131

--- Basic_Syntax/Example11.py
+++ Basic_Syntax/Example11.py
@@ -1,19 +1,18 @@
-"""
-text_file = open("text.txt", "w", encoding="utf8")
-print("text open", file=text_file)
-print("text save", file=text_file)
-print("text close", file=text_file)
-text_file.close()
-"""
 
-"""
-text_file = open("text.txt", "a", encoding="utf8")
-print("==========", file=text_file)
-text_file.write("text save2")
-text_file.close()
-"""
+# text_file = open("Basic_Syntax/text.txt", "w", encoding="utf8")
+# print("text open", file=text_file)
+# print("text save", file=text_file)
+# print("text close", file=text_file)
+# text_file.close()
 
-text_file = open("text.txt", "r", encoding="utf8")
+
+# text_file = open("Basic_Syntax/text.txt", "a", encoding="utf8")
+# print("==========", file=text_file)
+# text_file.write("text save2")
+# text_file.close()
+
+
+text_file = open("Basic_Syntax/text.txt", "r", encoding="utf8")
 # print(text_file.read())
 
 # print(text_file.readline(), end="")
--- Basic_Syntax/Example12.py
+++ Basic_Syntax/Example12.py
@@ -1,28 +1,28 @@
 import pickle
 
-# profile = open("profile.pickle", "wb")
-# info = {"name":"Kim", "age":29, "hobby":["book", "sleep", "run"]}
-# print(info)
-# pickle.dump(info, profile)
-# profile.close()
+profile = open("Basic_Syntax/profile.pickle", "wb")
+info = {"name":"Kim", "age":29, "hobby":["book", "sleep", "run"]}
+print(info)
+pickle.dump(info, profile)
+profile.close()
 
 
-# profile = open("profile.pickle", "rb")
-# info = pickle.load(profile)
-# print(info)
-# print(info["name"])
-# print(info["hobby"])
-# profile.close()
+profile = open("profile.pickle", "rb")
+info = pickle.load(profile)
+print(info)
+print(info["name"])
+print(info["hobby"])
+profile.close()
 
 
-with open("profile.pickle", "rb") as profile:
-    print(pickle.load(profile))
+# with open("profile.pickle", "rb") as profile:
+#     print(pickle.load(profile))
 
 
-with open("text2.txt", "w", encoding="utf8") as text2:
-    text2.write("open text2\n")
-    text2.write("save text2\n")
-    text2.write("close text2")
+# with open("text2.txt", "w", encoding="utf8") as text2:
+#     text2.write("open text2\n")
+#     text2.write("save text2\n")
+#     text2.write("close text2")
 
 
 
